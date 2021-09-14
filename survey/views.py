b9c872cc58d9a42f97a81d0fed8726d38bd9d4ef
Kirkey Psalms <kirkey_psalms@yahoo.com.ph>
2021-09-14 09:44:22 +0800

    survey information posting

--- survey/views.py
+++ survey/views.py
@@ -1,25 +1,23 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .forms import SurveyForm
+from .forms import createSurveyForm
 from .models import *
 
 # Create your views here.
-# def c(request):
-#     return HttpResponse('This is a survey')
-
 def index(request):
-    return render(request, 'surveys.html', {})
-    
+    return render(request, 'surveys.html',{})
+
 def createsurvey(request):
     if request.method == 'POST':
-        # print('sending survey information', request.POST)
-        form = SurveyForm(request.POST)
+        form = createSurveyForm(request.POST)
+        print(form)
+
         if form.is_valid():
             form.save()
             return redirect('/')
-    
-    form = SurveyForm()
-    context = {'form': form}
-    
-    return render(request, 'createsurvey.html', context)
+    else:
+        form = createSurveyForm()
+        context = {'form': form}
+        
+        return render(request, 'createsurvey.html', context)
 
